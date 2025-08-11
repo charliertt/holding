1e0c7cf7263a74c2fd94d8bce3525fc6fd911b75
charliertt <carlitosviveros588@gmail.com>
2025-08-11 13:14:24 -0500

    host

--- holding/settings.py
+++ holding/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-##b!=s%$_7pg37-!%d1=px(7o15a+g!t^tjpqd#etm9=x05y=(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
